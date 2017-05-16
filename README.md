46fa1e740faf4bac664a297462c6f09d90bfd907
Joey Lee <josephkanglee@gmail.com>
2017-05-16 11:36:16 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Transit Glow Light
+
+## About
+The Transit Glow Light is a light installation that uses varying intensities of light to indicate to people when they should leave their homes to catch the public transit near them. The light is actuated by real-time data from the [VAG](https://www.vag.de/) - the Neurnberg public transit agency.
+
+The project was supported and completed as part of the [Hackbay Hackathon](https://www.hackbay.de/) by [Zollhof](https://www.zollhof.de/). 
+
+## Setup:
+
+```
+npm install
+npm start
+```
---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var _ = require('underscore');
+var path = require("path");
+
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+    extended: true
+}));
+
+app.get("/", function(req, res) {
+	res.send("hello World");
+})
+
+
+app.get("/:home", function(req, res) {
+	res.send(req.params.home);
+})
+
+// set the app to listen at 3000
+app.listen(3000, function() {
+	console.log('listening on 3000')
+})
+
+
+/*
+
+setInterval() --> every 30 seconds & interpolate ==> then update the model
+
+Websockets 
+
+*/
