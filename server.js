ae0896d05f7e5a4be4dec8b10e9ad69556665037
Joey Lee <josephkanglee@gmail.com>
2017-05-17 14:22:48 +0200

    added web socket server side

--- server.js
+++ server.js
@@ -4,24 +4,54 @@ var bodyParser = require('body-parser');
 var _ = require('underscore');
 var path = require("path");
 
+
+var socket = require('socket.io');
+var server = app.listen(3000);
+var io = socket(server);
+
+app.use(express.static('public'));
+io.sockets.on('connection', newConnection);
+
+
+
 app.use(bodyParser.json()); // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
     extended: true
 }));
 
 app.get("/", function(req, res) {
-	res.send("hello World");
+  res.send("hello World");
 })
 
 
 app.get("/:home", function(req, res) {
-	res.send(req.params.home);
+  res.send(req.params.home);
 })
 
-// set the app to listen at 3000
-app.listen(3000, function() {
-	console.log('listening on 3000')
-})
+
+function newConnection(socket){
+  console.log('new connection: ' + socket.id);
+
+  console.log("number of sockets:", socketCount);
+  socket.on('mouse', mouseMsg);
+  socket.on('disconnect', disconnectMsg);
+
+  function mouseMsg(data){
+    socket.broadcast.emit('mouse', data);
+    // io.sockets.emit('mouse', data);
+  }
+
+  function disconnectMsg(){
+    console.log("disconnected! " + socket.id);
+  }
+}
+
+
+
+// // set the app to listen at 3000
+// app.listen(3000, function() {
+//  console.log('listening on 3000')
+// })
 
 
 /*
